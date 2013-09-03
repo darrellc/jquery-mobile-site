94afba20ced98dc7937ac38f77af6516d0422a99
Darrell Cardwell <darrell.cardwell@gmail.com>
2013-09-03 13:40:59 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChurchAppMobile::Application.config.secret_token = '1ed5ca23eae760c8352221d91ba046a1af44be3972a341512ed23230c9ddbf7c345264b63292cd7793cdf404e06a231f503ed458d5dd2764c9f391f64e31c367'
